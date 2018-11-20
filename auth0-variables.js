54a2a183f320ca3b6576028d5bbf5dcd41c7c225
tbricca <tbricca@gmail.com>
2018-11-20 11:05:07 -0800

    first commit

---
+++ auth0-variables.js
@@ -0,0 +1,5 @@
+var AUTH0_CLIENT_ID = 'HARmSNkii3uaxjDbwVRXghmGmEmTIKIP';
+var AUTH0_DOMAIN = 'tbricca.auth0.com';
+var AUTH0_AUDIENCE = 'YOUR_API_IDENTIFIER';
+var AUTH0_CALLBACK_URL = location.href;
+
