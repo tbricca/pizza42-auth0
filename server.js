18f31dbf3654d49892133dedb16da341a36d1754
tbricca <tbricca@gmail.com>
2018-11-21 14:42:19 -0800

    modified port for heroku

--- server.js
+++ server.js
@@ -37,5 +37,6 @@ app.get('/api/private', checkJwt, checkScopes, function(req, res) {
   res.json({ message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this." });
 });
 
-app.listen(3001);
-console.log('Listening on http://localhost:3001');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
