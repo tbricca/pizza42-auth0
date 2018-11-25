1d8e5d1b19f58d1a89e70d8ab03f20a98dace79e
tbricca <tbricca@gmail.com>
2018-11-25 12:42:20 -0800

    fixing heroku deployment issue

--- server.js
+++ server.js
@@ -44,3 +44,14 @@ app.get('/api/private', checkJwt, checkScopes, function(req, res) {
 app.listen(PORT, function() {
   console.log('port is up and running')
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
